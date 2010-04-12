19cb355d881d5528e652342ff20ba2da1d8a7c8c
Jude Sutton <jude.sutton@gmail.com>
2010-04-12 16:13:23 +0100

    removing the requirement that we have an array of fixnums

--- ext/fft_api.c
+++ ext/fft_api.c
@@ -9,22 +9,14 @@
 
 // Validate the incoming array
 static int validate( VALUE self ) {
-    int    i;
     int    len;
-    VALUE *values;
-    
+
     // Check we're an Array
     Check_Type( self, T_ARRAY );
 
     // make sure the size of the array is a power of 2
     len = RARRAY_LEN( self );
-    if( ( len < 2 ) || ( len & ( len − 1 ) ) ) return FALSE;
-        
-    // go through the array and check for FIXNUM_P(value)
-    values = RARRAY_PTR( self );
-    for( i = 0; i < RARRAY_LEN( self ); i++ ) {
-        if( !FIXNUM_P( values[i] ) ) return FALSE;
-    }
+    if( ( len < 2 ) || ( len & ( len - 1 ) ) ) return FALSE;
 
     return TRUE;
 }
@@ -33,7 +25,7 @@ static int validate( VALUE self ) {
 static VALUE method_fft( VALUE inArray ){
     int i;
     VALUE *values;
-    int *transformed = malloc( sizeof( int ) * RARRAY_LEN( inArray ) );
+    double *transformed = malloc( sizeof( double ) * RARRAY_LEN( inArray ) );
     VALUE outArray = rb_ary_new2( RARRAY_LEN( inArray ) );
 
     if( !validate( inArray ) ) {
@@ -44,12 +36,12 @@ static VALUE method_fft( VALUE inArray ){
     values = RARRAY_PTR( inArray );
     for( i = 0; i < RARRAY_LEN( inArray ); i++ ) {
         // process values
-        transformed[i] = FIX2INT( values[i] );
+        transformed[i] = NUM2DBL( values[i] );
     }
     
     // convert the integers into fixnums and stick them into a ruby array
     for( i = 0; i < RARRAY_LEN( inArray ); i++ ){
-        rb_ary_push( outArray, INT2FIX( transformed[i] ));
+        rb_ary_push( outArray, DBL2NUM( transformed[i] ));
     }
     
     free( transformed );
